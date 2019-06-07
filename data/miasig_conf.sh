978eea78bb8827e61ad5ae323ff8fa8144a861b8
Minji Kim <minji011@gmail.com>
2019-06-07 18:46:29 -0400

    Add files via upload

---
+++ data/miasig_conf.sh
@@ -0,0 +1,13 @@
+### Config file for MIA-Sig
+
+## Location of scripts
+miasig_dir="/projects/kimm/miasig/bin/"
+
+## Location of juicer
+juicer_loc="/opt/compsci/juicer/1.7.5/"
+
+## Data directory
+# Must contain <ref_genome>.chrom.sizes 
+data_dir="/projects/kimm/miasig/data/"
+
+
